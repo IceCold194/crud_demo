2554cad9d7853ae2216ebf0a5a10a146ac85abeb
Ice713 <icramzilruiz@gmail.com>
2025-11-25 08:17:17 +0800

    Tinago ang ID sa DataGridView

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -36,13 +36,14 @@ Public Class Form1
     End Sub
 
     Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
-        Dim query As String = "SELECT name, age, email FROM crud_demo_db.students_tbl;"
+        Dim query As String = "SELECT * FROM crud_demo_db.students_tbl;"
         Try
             Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
                 Dim adapter As New MySqlDataAdapter(query, conn) ' Get from Database
                 Dim table As New DataTable() ' Table Object
                 adapter.Fill(table) ' From Adapter to Table Object
                 DataGridView1.DataSource = table ' Display to DataGridView
+                DataGridView1.Columns("id").Visible = False
             End Using
         Catch ex As Exception
             MsgBox(ex.Message)
